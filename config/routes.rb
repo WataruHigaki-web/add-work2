ca4250c4f479cdf43343179813be81c0d3ebcc5a
WataruHigaki-web <wataruhigaki@gmail.com>
2020-03-05 11:47:13 +0000

    create favorite

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root 'home#top'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  
+  resources :books do
+    resources :favorites, only: [:create, :destroy]
+  end
+
   get 'home/about'
 end
